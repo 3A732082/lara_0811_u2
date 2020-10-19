2f6fe143f9a8d0330f6d22bbaf50a116eb4ce6c2
林江閤/tkmk7q8qq@gmail.com <elaine@ncut.edu.tw>
2020-10-19 17:03:53 +0800

    練習7 有問題

--- routes/web.php
+++ routes/web.php
@@ -12,10 +12,13 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+/*
 Route::get('/', function () {
     return view('welcome');
 });
+*/
+Route::get('/',['as'=>'home.lndex','uses'=>'HomeController@index']);
+
 Route::get('r1', function () {
     return redirect('r2');
 });
