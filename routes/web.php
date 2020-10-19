7d822907caec9231172e18f1ea46a5406ce7e9a2
林江閤/tkmk7q8qq@gmail.com <elaine@ncut.edu.tw>
2020-10-19 16:29:13 +0800

    add route

--- routes/web.php
+++ routes/web.php
@@ -27,3 +27,8 @@ Route::get('r2', function () {
 Route::get('hello/{name?}',function($name='Everybody'){
     return'Hello,'.$name;
 });
+
+Route::get('r3', function () {
+    return view('welcome');
+
+});
